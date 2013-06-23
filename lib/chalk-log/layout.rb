0f0e519594669c3ac444540cbacef0873aa0c1e8
Evan Broder <evan@stripe.com>
2013-06-22 20:43:07 -0700

    Handle a nil exception peacefully
    I don't think we should be asserting if the exception passed to
    log.error happens to be nil - we'll just not display it.
    
    /cc @gdb for review

--- lib/chalk-log/layout.rb
+++ lib/chalk-log/layout.rb
@@ -16,7 +16,7 @@ class Chalk::Log::Layout < ::Logging::Layout
 
     # Data provided by blocks may not be arrays yet
     data = [data] unless data.kind_of?(Array)
-    while [nil, true, false].include?(data.last)
+    while data.length > 2 && [nil, true, false].include?(data.last)
       maybe_assert(false, "Ignoring deprecated arguments passed to logger: #{data.inspect}")
       data.pop
     end
