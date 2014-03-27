d36da4a5c57e02f9b215d31ed0b01f72f0531619
Greg Brockman <gdb@gregbrockman.com>
2014-03-27 16:47:51 -0700

    Remove autotruncating
    We should probably bring it back in a smarter form, but for now it
    both sometimes covers up useful lines, and it also introduces some
    Chalk::Config stuff that needs to be upgraded.

--- lib/chalk-log/utils.rb
+++ lib/chalk-log/utils.rb
@@ -1,24 +1,8 @@
 module Chalk::Log::Utils
+  # TODO: maybe add some support for autotruncating? Would need
+  # some way of finding library line
   def self.format_backtrace(backtrace)
-    if chalk_config_development? && (depth = Chalk::Log::Config[:backtrace_depth])
-      non_library_line_found = false
-      remaining_lines = depth
-      lines = []
-      backtrace.each do |line|
-        break if remaining_lines == 0
-        non_library_line_found ||= non_library_line?(line)
-        remaining_lines -= 1 if non_library_line_found
-        lines << line
-      end
-
-      if lines.length < backtrace.length
-        lines << "[backtrace truncated: #{lines.length} / #{backtrace.length} lines]"
-      end
-    else
-      lines = backtrace
-    end
-
-    "  " + lines.join("\n  ")
+    "  " + backtrace.join("\n  ")
   end
 
   def self.explode_nested_hash(hash, prefix = [])
@@ -32,27 +16,5 @@ module Chalk::Log::Utils
       end
     end
     exploded_hash
-  end
-
-  def self.non_library_line?(line)
-    # Relative paths are never library lines
-    return true unless line.start_with?('/')
-
-    if chalk_config_development?
-      # An absolute path starting with the basedir is also not a
-      # library line.
-      base = Chalk::Tools::PathUtils.path
-      line.start_with?(base)
-    else
-      false
-    end
-  end
-
-  def self.chalk_config_loaded?
-    defined?(Chalk::Config) && Chalk::Config.initialized?
-  end
-
-  def self.chalk_config_development?
-    chalk_config_loaded? && Chalk::Config.development?
   end
 end
